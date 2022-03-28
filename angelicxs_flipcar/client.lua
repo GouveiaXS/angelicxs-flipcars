fffe2c737b3356d14ffa35ddc7d365d383e0cff0
GouveiaXS <99211701+GouveiaXS@users.noreply.github.com>
2022-03-27 18:51:54 -0600

    QBCore compatability, and additions of a config.

--- angelicxs_flipcar/client.lua
+++ angelicxs_flipcar/client.lua
@@ -1,74 +1,75 @@
 ESX = nil
+QBcore = nil
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
+RegisterNetEvent('angelicxs-flipcar:Notify', function(message, type)
+	if Config.UseCustomNotify then
+        TriggerEvent('angelicxs-flipcar:CustomNotify',message, type)
+	elseif Config.UseESX then
+		ESX.ShowNotification(message)
+	elseif Config.UseQBCore then
+		QBCore.Functions.Notify(message, type)
+	end
+end)
+
+CreateThread(function()
+    if Config.UseESX then
+        while ESX == nil do
+            TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+            Wait(0)
+        end
+        while not ESX.IsPlayerLoaded() do
+            Wait(100)
+        end
+    elseif Config.UseQBCore then
+        QBCore = exports['qb-core']:GetCoreObject()
+    end
+
+    if Config.UseChatCommand then
+        RegisterCommand(Config.ChatCommand, function()
+            TriggerEvent('angelicxs-flipcar:flipcar')
+        end)
     end
 end)
 
-RegisterNetEvent('angelicxs:flipcar')
-AddEventHandler('angelicxs:flipcar', function()
+RegisterNetEvent('angelicxs-flipcar:flipcar')
+AddEventHandler('angelicxs-flipcar:flipcar', function()
     local ped = PlayerPedId()
-    local VehicleData, dist = ESX.Game.GetClosestVehicle()
-    if dist <= 5 then
-     --   NetworkRequestControlOfEntity(VehicleData)
+    local pedcoords = GetEntityCoords(ped)
+    local VehicleData = GetVehiclePedIsIn(ped, true)
+    local dist = #(pedcoords - GetEntityCoords(VehicleData))
+    local inside = IsPedInAnyVehicle(ped, true)
+    if dist <= 3 and not inside then
         RequestAnimDict('missfinale_c2ig_11')
         TaskPlayAnim(ped, 'missfinale_c2ig_11', 'pushcar_offcliff_m', 2.0, -8.0, -1, 35, 0, 0, 0, 0)
-        exports['mythic_notify']:SendAlert('success', "Attempting to Flip Car!")
-        exports['mythic_progbar']:Progress({
-            name = "flip_car",
-            duration = 10000,
-            label = 'Flipping Car ...',
-            useWhileDead = false,
-            canCancel = false,
-            controlDisables = {
-                disableMovement = true,
-                disableCarMovement = true,
-                disableMouse = true,
-                disableCombat = true,
-            },
-        }, function(cancelled)
-            if not cancelled then
-                exports['mythic_notify']:SendAlert('success', "Your car has been flipped!") 
-                local carCoords = GetEntityCoords(VehicleData)
-                SetEntityCoords(VehicleData, carCoords.x, carCoords.y, carCoords.z+0.1, false, false, false, true)
-                SetVehicleOnGroundProperly(VehicleData)
-                ESX.Game.Teleport(VehicleData, carCoords)
-                ClearPedTasks(ped)
-            else
-                exports['mythic_notify']:SendAlert('error', "Something went wrong!") 
-                ClearPedTasks(ped)
-            end
-        end)
+        Wait(Config.TimetoFlip*1000)
+        local carCoords = GetEntityCoords(VehicleData)
+        SetEntityCoords(VehicleData, carCoords.x, carCoords.y, (carCoords.z+0.1), false, false, false, true)
+        SetVehicleOnGroundProperly(VehicleData)
+        TriggerEvent('angelicxs-flipcar:Notify', Config.Lang['flipped'], Config.LangType['success'])
+        ClearPedTasks(ped)
+    elseif inside then
+        TriggerEvent('angelicxs-flipcar:Notify', Config.Lang['in_vehicle'], Config.LangType['error'])
     else
-        exports['mythic_notify']:SendAlert('error', "You are not close enough to flip!") 
+        TriggerEvent('angelicxs-flipcar:Notify', Config.Lang['far_away'], Config.LangType['error'])
     end
 end)
 
-RegisterCommand('flipcar', function()
-    TriggerEvent('angelicxs:flipcar')
-end)
-
-
--- Draw markers and more
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-        local playerPed = PlayerPedId()
-        local playerCoords = GetEntityCoords(playerPed)
- --       local vehicle, dist = ESX.Game.GetClosestVehicle()
- --       if dist <=5 then
-            exports.qtarget:Vehicle({
+CreateThread(function()
+    if Config.UseThirdEye then
+        while true do
+            Wait(0)
+            local playerPed = PlayerPedId()
+            local playerCoords = GetEntityCoords(playerPed)
+            exports[Config.ThirdEyeName]:Vehicle({
                 options = {
                     {
-                        event = "angelicxs:flipcar",
+                        event = "angelicxs-flipcar:flipcar",
                         icon = "fas fa-arrow-up",
                         label = "Flip Vehicle",
-                        num = 3,
                         distance = 2
                     },
                 },
             })
-	end
+        end
+    end
 end)
